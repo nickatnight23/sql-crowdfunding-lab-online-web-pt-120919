5ed3e7f4a120370021289eed7017f894f4874be3
Latricia <tricianickelberry@gmail.com>
2020-02-24 20:50:49 -0800

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,60 @@
+INSERT INTO projects VALUES (title, category, funding_goal, start_date, end_date) VALUES ("dog homes","Animal",500,"2/25/2020","3/25/2020")
+INSERT INTO projects VALUES (title, category, funding_goal, start_date,end_date) VALUES ("Mental Health","Well-being",800,"2/25/2020","3/25/2020");
+INSERT INTO projects VALUES (title, category, funding_goal, start_date,end_date) VALUES ()
+INSERT INTO projects VALUES (title, category, funding_goal, start_date,end_date) VALUES ()
+INSERT INTO projects VALUES (title, category, funding_goal, start_date,end_date) VALUES ()
+INSERT INTO projects VALUES (title, category, funding_goal, start_date,end_date) VALUES ()
+INSERT INTO projects VALUES (title, category, funding_goal, start_date,end_date) VALUES ()
+INSERT INTO projects VALUES (title, category, funding_goal, start_date,end_date) VALUES ()
+INSERT INTO projects VALUES (title, category, funding_goal, start_date,end_date) VALUES ()
+INSERT INTO projects VALUES (title, category, funding_goal, start_date,end_date) VALUES ()
+
+INSERT INTO users (name, age) VALUES ('Jimmy Bloom', 27);
+INSERT INTO users (name, age) VALUES ('Toya Thompson', 34);
+INSERT INTO users (name, age) VALUES ('Michael Davis', 45);
+INSERT INTO users (name, age) VALUES ('Harry Potter', 18);
+INSERT INTO users (name, age) VALUES ('Sarah Cotter', 36);
+INSERT INTO users (name, age) VALUES ('Cindy Crowford', 65);
+INSERT INTO users (name, age) VALUES ('Dominque Jackson',23);
+INSERT INTO users (name, age) VALUES ('Lacy Simpson', 33);
+INSERT INTO users (name, age) VALUES ('Erica Johnson', 42);
+INSERT INTO users (name, age) VALUES ('Jerry Brown', 56);
+INSERT INTO users (name, age) VALUES ('Tina Turner', 40);
+INSERT INTO users (name, age) VALUES ('Gary Hickson', 33);
+INSERT INTO users (name, age) VALUES ('Michael Douglas', 67);
+INSERT INTO users (name, age) VALUES ('Lisa Stamp', 22);
+INSERT INTO users (name, age) VALUES ('Terry Stamper, 57);
+INSERT INTO users (name, age) VALUES ('Glade Map', 44);
+INSERT INTO users (name, age) VALUES ('Eddie Rocker', 21);
+INSERT INTO users (name, age) VALUES ('Laura Santiago', 36);
+INSERT INTO users (name, age) VALUES ('Betty Anderson', 70);
+INSERT INTO users (name, age) VALUES ('Tasha Harington', 25);
+
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
+INSERT INTO pledges (amount,user_id,project_id) VALUES ()
